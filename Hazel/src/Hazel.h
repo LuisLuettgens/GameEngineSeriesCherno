8b200dcf7be3863895cbad14cec8a3c8b613463a
Luis Lüttgens <luis1@uni-bremen.de>
2018-11-22 15:41:06 +0100

    Everything working until video EntryPoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#ifndef HAZEL_H
+#define HAZEL_H
+
+    // For use by Hazel applications
+    #include "Hazel/Application.hpp"
+
+    // ---Entry Point--------------------
+    #include "Hazel/EntryPoint.hpp"
+    // ----------------------------------
+
+#endif
---
+++ Hazel/src/Hazel/EntryPoint.hpp
@@ -0,0 +1,17 @@
+#ifndef ENTRYPOINT_HPP
+#define ENTRYPOINT_HPP
+
+    #ifdef HZ_PATFORM_WINDOWS
+    #else
+
+        extern Hazel::Application* Hazel::CreateApplication();
+
+        int main(int argc, char** argv)
+        {
+            auto app = Hazel::CreateApplication();
+            app->Run();
+            delete app;
+        }
+    #endif
+
+#endif
